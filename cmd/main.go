a05b5f42459368a8d88d9065c2bc8e6b8e0698d6
Bradley Boutcher <btboutcher@icloud.com>
2020-07-10 14:15:53 -0400

    Add secret retrieval methods
    - Includes flags that are consistent with summon providers
    - Singular method for retrieving a secret
    - retrieves env variables used for creating an authenticated
    client
    - Includes debug flag for adding a logger to the request

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,43 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
-	"github.com/cyberark/summon-keyvault/pkg/summon_keyvault"
+	"net/http"
+	"net/http/httputil"
 	"os"
 
-	"github.com/cyberark/conjur-api-go/conjurapi"
+	"github.com/cyberark/summon-keyvault/pkg/summon_keyvault"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/keyvault/keyvault"
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/auth"
+	"github.com/Azure/go-autorest/autorest"
 	"github.com/karrick/golf"
 	log "github.com/sirupsen/logrus"
 )
+/*
+ * You need to set four environment variables before using the app:
+ * AZURE_TENANT_ID: Your Azure tenant ID
+ * AZURE_CLIENT_ID: Your Azure client ID. This will be an app ID from your AAD.
+ * AZURE_CLIENT_SECRET: The secret for the client ID above.
+ * KVAULT: The name of your vault (just the name, not the full URL/path)
+ *
+ * Usage
+ * Get the value for a secret in the vault:
+ * cmd YOUR_SECRETS_NAME
+ *
+ */
 
-func RetrieveSecret(variableName string) {
-	config, err := conjurapi.LoadConfig()
-	if err != nil {
-		log.Errorf("Failed loading Conjur API config: %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	conjur, err := conjurapi.NewClientFromEnvironment(config)
-	if err != nil {
-		log.Errorf("Failed creating a Conjur client: %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	value, err := conjur.RetrieveSecret(variableName)
-	if err != nil {
-		log.Errorln(err.Error())
-		os.Exit(1)
-	}
-
-	os.Stdout.Write([]byte(value))
-
-}
+var (
+	vaultName string
+)
 
 func main() {
 	var help = golf.BoolP('h', "help", false, "show help")
 	var version = golf.BoolP('V', "version", false, "show version")
 	var verbose = golf.BoolP('v', "verbose", false, "be verbose")
+    var setDebug  = golf.BoolP('d', "debug", false, "use debug")
 
 	golf.Parse()
 	args := golf.Args()
@@ -59,5 +60,74 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	RetrieveSecret(args[0])
+	PassManager(args[0], setDebug)
+}
+
+func PassManager(secret string, setDebug *bool) {
+	if os.Getenv("AZURE_TENANT_ID") == "" || os.Getenv("AZURE_CLIENT_ID") == "" ||
+		os.Getenv("AZURE_CLIENT_SECRET") == "" || os.Getenv("KVAULT") == "" {
+		fmt.Println("env vars not set, exiting...")
+		os.Exit(1)
+	}
+	vaultName = os.Getenv("KVAULT")
+
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		fmt.Printf("unable to create vault authorizer: %v\n", err)
+		os.Exit(1)
+	}
+
+	basicClient := keyvault.New()
+	basicClient.Authorizer = authorizer
+
+	if *setDebug {
+		basicClient.RequestInspector = logRequest()
+		basicClient.ResponseInspector = logResponse()
+	}
+
+	if flag.NArg() == 1 && flag.NFlag() <= 0 {
+		getSecret(basicClient, secret)
+	}
+}
+
+func getSecret(basicClient keyvault.BaseClient, secretName string) {
+	secretResp, err := basicClient.GetSecret(
+		context.Background(),
+		fmt.Sprintf("https://%s.vault.azure.net", vaultName),
+		secretName,
+		"")
+
+	if err != nil {
+		fmt.Printf("unable to get value for secret: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(*secretResp.Value)
+}
+
+func logRequest() autorest.PrepareDecorator {
+	return func(p autorest.Preparer) autorest.Preparer {
+		return autorest.PreparerFunc(func(r *http.Request) (*http.Request, error) {
+			r, err := p.Prepare(r)
+			if err != nil {
+				log.Println(err)
+			}
+			dump, _ := httputil.DumpRequestOut(r, true)
+			log.Println(string(dump))
+			return r, err
+		})
+	}
+}
+
+func logResponse() autorest.RespondDecorator {
+	return func(p autorest.Responder) autorest.Responder {
+		return autorest.ResponderFunc(func(r *http.Response) error {
+			err := p.Respond(r)
+			if err != nil {
+				log.Println(err)
+			}
+			dump, _ := httputil.DumpResponse(r, true)
+			log.Println(string(dump))
+			return err
+		})
+	}
 }
